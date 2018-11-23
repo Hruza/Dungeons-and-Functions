2f201daec8bb925090acd6a1c5be6ba13f406181
Hruza <hruza.jan1@gmail.com>
2018-11-23 12:33:07 +0100

    Setup

---
+++ Assets/Level/Control/Player movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Posune objekt ve směru dir ve 2d.
+    /// </summary>
+    /// <param name="dir"></param>
+    public void Move(Vector2 dir) {
+
+ 
+    }
+}
