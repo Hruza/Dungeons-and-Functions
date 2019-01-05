1412fa130f12c19ff6617970b6f7962b10e23794
Martin Vejvoda <martin.vejvoda@email.cz>
2019-01-05 23:04:16 +0100

    Přidání itemů
    Přidal jsem třídu reprezentující jednotlivé předměty (Item) a třídu, která spravuje ty, které má hráč právě oblečeny (EquipManager). Trochu jsem upravil třídu Player.

--- Assets/Level/Camera/Follow.cs
+++ Assets/Level/Camera/Follow.cs
@@ -6,15 +6,19 @@ public class Follow : MonoBehaviour {
 
     public float followSpeed=0.8f;
 
+    /// <summary>
+    /// cíl, který bude následován
+    /// </summary>
     private Vector3 target;
 
 	// Use this for initialization
 	void Start () {
-	}
+        target = Player.player.transform.position;
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        target = Player.player.transform.position;
+        
         transform.position = Vector3.Lerp(transform.position, target, followSpeed);
 	}
 
---
+++ Assets/Level/Control/Item.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Třída reprezentující předmět, který může hráč získat.
+/// </summary>
+public class Item : MonoBehaviour
+{
+    /// <summary>
+    /// výčtový typ určující typ předmětu
+    /// </summary>
+    public enum ItemType { MeeleWeapon, RangeWeapon, Armor}
+    /// <summary>
+    /// typ předmětu
+    /// </summary>
+    public ItemType Type { get; private set; }
+    /// <summary>
+    /// brnění, má smysl pouze pro brnění (a případně štíty, pokud budou)
+    /// </summary>
+    public int Armor { get; private set; }
+    /// <summary>
+    /// o kolik se zvýší brnění hráče (v absolutních číslech)
+    /// </summary>
+    public int BonusArmorAdditive { get; private set; }
+    /// <summary>
+    /// o kolik se zvýší brnění hráče (v procentech)
+    /// </summary>
+    public int BonusArmorMultiplicative { get; private set; }
+    /// <summary>
+    /// poškození předmětu, má smysl pouze u zbraní
+    /// </summary>
+    public int Damage { get; private set; }
+    /// <summary>
+    /// o kolik se zvýší celkové poškození hráče (aditivní)
+    /// </summary>
+    public int BonusDamageAdditive { get; private set; }
+    /// <summary>
+    /// o kolik se zvýší poškození hráče (v procentech)
+    /// </summary>
+    public int BonusDamageMultiplicative { get; private set; }
+    /// <summary>
+    /// o kolik se zvýší hráčova regenerace
+    /// </summary>
+    public int Regeneration { get; private set; }
+
+    private void Start()
+    {
+        //nějaká základní inicializace
+        Armor = 0;
+        BonusArmorAdditive = 0;
+        BonusArmorMultiplicative = 0;
+        Damage = 0;
+        BonusDamageAdditive = 0;
+        BonusDamageMultiplicative = 0;
+        Regeneration = 0;
+    }
+}
