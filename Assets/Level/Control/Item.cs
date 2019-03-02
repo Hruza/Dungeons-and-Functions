388f70552eb9b3979bd27f2156d7252622ee79d6
Martin Vejvoda <martin.vejvoda@email.cz>
2019-03-02 15:55:26 +0100

    tak snad

--- Assets/Level/Control/Item.cs
+++ Assets/Level/Control/Item.cs
@@ -1,8 +1,30 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
+<<<<<<< Updated upstream
+=======
+/// všechny možné typy předmětů
+/// </summary>
+public enum ItemType { Armor, Weapon };
+/// <summary>
+/// všechny možné typy zbraní
+/// </summary>
+public enum WeaponType { Meele, Ranged };
+/// <summary>
+/// možné kvality předmětů
+/// </summary>
+public enum Quality { Basic, C, Cplusplus, Csharp };
+
+/// <summary>
+/// možné rarity zbraní
+/// </summary>
+public enum Rarity { Common, Rare, Unique };
+
+/// <summary>
+>>>>>>> Stashed changes
 /// Třída reprezentující předmět, který může hráč získat.
 /// </summary>
 public class Item : MonoBehaviour
@@ -12,7 +34,76 @@ public class Item : MonoBehaviour
     /// </summary>
     public enum ItemType { MeeleWeapon, RangeWeapon, Armor}
     /// <summary>
+<<<<<<< Updated upstream
     /// typ předmětu
+=======
+    /// úroveň předmětu
+    /// </summary>
+    public int itemLevel;
+
+    /// <summary>
+    /// bezparametrický kontruktor (nic neudělá)
+    /// </summary>
+    public Item()
+    {
+    }
+
+    /// <summary>
+    /// kontruktor vytvářející generický item (obsahuje pouze úroveň, kvalitu a raritu předmětu)
+    /// </summary>
+    /// <param name="itemLevel"></param>
+    public Item(int itemLevel)
+    {
+        this.itemLevel = itemLevel;
+        name = "GenericItem";
+        quality = Probability.RandomQuality();
+        rarity = Probability.RandomRarity();
+    }
+
+    /// <summary>
+    /// statická metoda sloužící pro generování náhodného itemu
+    /// </summary>
+    public int itemLevel;
+
+    /// <summary>
+    /// bezparametrický kontruktor (nic neudělá)
+    /// </summary>
+    public Item()
+    {
+    }
+
+    /// <summary>
+    /// kontruktor vytvářející generický item (obsahuje pouze úroveň, kvalitu a raritu předmětu)
+    /// </summary>
+    /// <param name="itemLevel"></param>
+    public Item(int itemLevel)
+    {
+        this.itemLevel = itemLevel;
+        name = "GenericItem";
+        quality = Probability.RandomQuality();
+        rarity = Probability.RandomRarity();
+    }
+
+    /// <summary>
+    /// statická metoda sloužící pro generování náhodného itemu
+    /// </summary>
+    /// <param name="itemLevel">level vygenerovaného itemu (stejný, jako level monstra, ze kterého dropnul)</param>
+    /// <returns>vygenerovaný item</returns>
+    public static Item Generate(int itemLevel)
+    {
+        Item item = new Item(itemLevel);
+
+        //ToDo
+        Debug.Log("Ehm, zatím nic.");
+        return null;
+    }
+}
+
+public class ArmorItem : Item
+{
+    /// <summary>
+    /// brnění
+>>>>>>> Stashed changes
     /// </summary>
     public ItemType Type { get; private set; }
     /// <summary>
@@ -54,5 +145,145 @@ public class Item : MonoBehaviour
         BonusDamageAdditive = 0;
         BonusDamageMultiplicative = 0;
         Regeneration = 0;
+    }
+}
+
+
+/// <summary>
+/// Statická třída mající na starosti generování náhodných rarit a kvalit. Obsahuje i jednotilé pravděpodobnosti.
+/// </summary>
+public static class Probability
+{
+    /// <summary>
+    /// pravděpodobnosti, s jakými mohou být jednotlivé kvality vygenerovány
+    /// </summary>
+    private static Dictionary<Quality, double> qualityProbabilities = new Dictionary<Quality, double>
+    {
+        {Quality.Basic, 0.9 },
+        {Quality.C, 0.1 }
+    };
+
+    /// <summary>
+    /// pravděpodobnosti, s jakými mohou být jednotlivé rarity vygenerovány
+    /// </summary>
+    private static Dictionary<Rarity, double> rarityProbabilities = new Dictionary<Rarity, double>
+    {
+        {Rarity.Common, 0.85 },
+        {Rarity.Rare, 0.14 },
+        {Rarity.Unique, 0.1 }
+    };
+
+    /// <summary>
+    /// vygenerování náhodné kvality předmětu (pravděpodobnosti nejsou rozloženy stejnoměrně)
+    /// </summary>
+    /// <returns>náhodně vygenerovaná kvalita</returns>
+    public static Quality RandomQuality()
+    {
+        double total = 0;
+        foreach (double weight in qualityProbabilities.Values)
+            total += weight;
+
+        double random = (double)UnityEngine.Random.Range(0, 1);
+        double sum = 0;
+        foreach (Quality quality in Enum.GetValues(typeof(Quality)))
+        {
+            sum += qualityProbabilities[quality];
+            if (random <= sum)
+                return quality;
+        }
+
+        return Quality.Basic;
+    }
+
+    /// <summary>
+    /// vygenerování náhodné rarity předmětu (pravděpodobnosti rarit nejsou rozloženy náhodně)
+    /// </summary>
+    /// <returns>nááhodně vygenerovaná rarita</returns>
+    public static Rarity RandomRarity()
+    {
+        double total = 0;
+        foreach (double weight in rarityProbabilities.Values)
+            total += weight;
+
+        double random = (double)UnityEngine.Random.Range(0f, 1f);
+        double sum = 0;
+        foreach (Rarity rarity in Enum.GetValues(typeof(Rarity)))
+        {
+            sum += rarityProbabilities[rarity];
+            if (random <= sum)
+                return rarity;
+        }
+
+        return Rarity.Common;
+    }
+}
+
+
+/// <summary>
+/// Statická třída mající na starosti generování náhodných rarit a kvalit. Obsahuje i jednotilé pravděpodobnosti.
+/// </summary>
+public static class Probability
+{
+    /// <summary>
+    /// pravděpodobnosti, s jakými mohou být jednotlivé kvality vygenerovány
+    /// </summary>
+    private static Dictionary<Quality, double> qualityProbabilities = new Dictionary<Quality, double>
+    {
+        {Quality.Basic, 0.9 },
+        {Quality.C, 0.1 }
+    };
+
+    /// <summary>
+    /// pravděpodobnosti, s jakými mohou být jednotlivé rarity vygenerovány
+    /// </summary>
+    private static Dictionary<Rarity, double> rarityProbabilities = new Dictionary<Rarity, double>
+    {
+        {Rarity.Common, 0.85 },
+        {Rarity.Rare, 0.14 },
+        {Rarity.Unique, 0.1 }
+    };
+
+    /// <summary>
+    /// vygenerování náhodné kvality předmětu (pravděpodobnosti nejsou rozloženy stejnoměrně)
+    /// </summary>
+    /// <returns>náhodně vygenerovaná kvalita</returns>
+    public static Quality RandomQuality()
+    {
+        double total = 0;
+        foreach (double weight in qualityProbabilities.Values)
+            total += weight;
+
+        double random = (double)UnityEngine.Random.Range(0, 1);
+        double sum = 0;
+        foreach (Quality quality in Enum.GetValues(typeof(Quality)))
+        {
+            sum += qualityProbabilities[quality];
+            if (random <= sum)
+                return quality;
+        }
+
+        return Quality.Basic;
+    }
+
+    /// <summary>
+    /// vygenerování náhodné rarity předmětu (pravděpodobnosti rarit nejsou rozloženy náhodně)
+    /// </summary>
+    /// <returns>nááhodně vygenerovaná rarita</returns>
+    public static Rarity RandomRarity()
+    {
+        double total = 0;
+        foreach (double weight in rarityProbabilities.Values)
+            total += weight;
+
+        double random = (double)UnityEngine.Random.Range(0f, 1f);
+        double sum = 0;
+        foreach (Rarity rarity in Enum.GetValues(typeof(Rarity)))
+        {
+            sum += rarityProbabilities[rarity];
+            if (random <= sum)
+                return rarity;
+        }
+
+        return Rarity.Common;
     }
 }
---
+++ Assets/Level/Control/PossibleItem.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossibleItem : ScriptableObject
+{
+    /// <summary>
+    /// jméno předmětu
+    /// </summary>
+    public string name;
+    /// <summary>
+    /// minimální možný item level, který může předmět mít
+    /// </summary>
+    public int lowerItemLevel;
+    /// <summary>
+    /// maximální možný item level, který může předmět mít
+    /// </summary>
+    public int upperItemLevel;
+}
+
+public class PossibleWeapon : PossibleItem
+{
+    /// <summary>
+    /// dolní hranice minimálnoho poškození zbraně
+    /// </summary>
+    public int lowerMinDamage;
+    /// <summary>
+    /// horní hranice minimálnoho poškození zbraně
+    /// </summary>
+    public int upperMinDamage;
+    /// <summary>
+    /// dolní hranice maximálního poškození zbraně
+    /// </summary>
+    public int lowerMaxDamage;
+    /// <summary>
+    /// horní hranice maximálního poškození zbraně
+    /// </summary>
+    public int upperMaxDamage;
+    /// <summary>
+    /// rychlost útoku zbraně
+    /// </summary>
+    public int attackSpeed;
+
+    /// <summary>
+    /// databáze všech možných zbraní
+    /// </summary>
+    public static List<PossibleWeapon> AllPossibleWeapons = new List<PossibleWeapon>
+        {
+            new PossibleWeapon
+            {
+                name = "ItegralSword",
+                lowerItemLevel = 1,
+                upperItemLevel = 1000,
+                lowerMinDamage = 1,
+                upperMinDamage = 4,
+                lowerMaxDamage = 5,
+                upperMaxDamage = 10,
+                attackSpeed = 1
+            }
+        };
+}
+
+public class PossibleArmor : PossibleItem
+{
+    /// <summary>
+    /// minimální možné brnění
+    /// </summary>
+    public int lowerArmor;
+    /// <summary>
+    /// maximální možné brnění
+    /// </summary>
+    public int upperArmor;
+    /// <summary>
+    /// o kolik je zpomalen hráč, když má toto brnění
+    /// </summary>
+    public int movementSpeedReduction;
+
+    /// <summary>
+    /// databáze všech možných brnění
+    /// </summary>
+    public static List<PossibleArmor> AllPossibleArmors = new List<PossibleArmor>
+    {
+        new PossibleArmor
+        {
+            name = "BasicArmor",
+            lowerItemLevel = 1,
+            upperItemLevel = 1000,
+            lowerArmor = 1,
+            upperArmor = 5,
+            movementSpeedReduction = 0
+        }
+    };
+}
